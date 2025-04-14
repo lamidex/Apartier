06ed9a00332b1043b9b43619e57faa98f9170011
lamidex <olamidestephen81@gmail.com>
2025-04-14 14:50:46 -0700

    bug fix

---
+++ models/booking.js
@@ -0,0 +1,31 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../config/db');
+
+const Booking = sequelize.define('Booking', {
+  numberOfNights: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  totalAmount: {
+    type: DataTypes.DECIMAL(10, 2),
+    allowNull: false
+  },
+  paymentStatus: {
+    type: DataTypes.ENUM('PENDING', 'COMPLETED', 'FAILED'),
+    defaultValue: 'PENDING'
+  },
+  paymentReference: {
+    type: DataTypes.STRING
+  }
+});
+
+// Define relationships
+const User = require('./user');
+const Apartment = require('./apartment');
+
+Booking.belongsTo(User);
+Booking.belongsTo(Apartment);
+User.hasMany(Booking);
+Apartment.hasMany(Booking);
+
+module.exports = Booking;
