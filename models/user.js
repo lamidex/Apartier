c1613424707044cbb118b04db20458501a9fa72c
lamidex <olamidestephen81@gmail.com>
2025-04-14 14:37:24 -0700

    bug fix

---
+++ models/user.js
@@ -0,0 +1,23 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../config/db');
+
+const User = sequelize.define('User', {
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true
+    }
+  },
+  googleId: {
+    type: DataTypes.STRING,
+    unique: true
+  },
+  role: {
+    type: DataTypes.ENUM('USER', 'ADMIN'),
+    defaultValue: 'USER'
+  }
+});
+
+module.exports = User;
