876bc3ba97fc3145de491791211d2c360d184366
lamidex <olamidestephen81@gmail.com>
2025-04-14 18:19:23 -0700

    update

---
+++ middleware/auth.js
@@ -0,0 +1,8 @@
+const isAuthenticated = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      return next();
+    }
+    res.status(401).json({ error: 'Please log in to continue' });
+  };
+  
+  module.exports = { isAuthenticated };
