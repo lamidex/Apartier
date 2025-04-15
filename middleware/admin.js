28588066b1ae37ef5980955186c13a79ed8d96f9
lamidex <olamidestephen81@gmail.com>
2025-04-14 18:25:13 -0700

    bug fix

---
+++ middleware/admin.js
@@ -0,0 +1,8 @@
+const isAdmin = (req, res, next) => {
+    if (req.user && req.user.role === 'ADMIN') {
+      return next();
+    }
+    res.status(403).json({ error: 'Admin access required' });
+  };
+  
+  module.exports = { isAdmin };
