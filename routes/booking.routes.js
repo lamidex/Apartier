6eaf1289eb2ad294dfd538590eec8f3434520099
lamidex <olamidestephen81@gmail.com>
2025-04-14 18:43:13 -0700

    updated

---
+++ routes/booking.routes.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const bookingController = require('../controllers/booking.controller');
+const { isAuthenticated } = require('../middleware/auth');
+
+router.post('/bookings', isAuthenticated, bookingController.createBooking);
+
+
+router.get('/bookings/verify/:reference', isAuthenticated, bookingController.verifyPayment);
+
+module.exports = router;
