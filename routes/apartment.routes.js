d4323733ba17d7b5d16a2f9cb886c9f653a0c8aa
lamidex <olamidestephen81@gmail.com>
2025-04-14 18:36:17 -0700

    update

---
+++ routes/apartment.routes.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const apartmentController = require('../controllers/apartment.controller');
+const { isAdmin } = require('../middleware/admin');
+const upload = require('../middleware/upload');
+
+router.get('/apartments', apartmentController.getAllApartments);
+router.post('/apartments', isAdmin, upload.fields([
+  { name: 'image1', maxCount: 1 },
+  { name: 'image2', maxCount: 1 }
+]), apartmentController.createApartment);
+router.get('/apartments/state/:state', apartmentController.getByState);
+router.get('/apartments/available', apartmentController.getTotalAvailable);
+
+module.exports = router;
